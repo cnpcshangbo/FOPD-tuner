182624b29e462a9b55603017f9fd70389fdebac8
Bo Shang <cnpcshangbo@gmail.com>
2017-08-15 21:48:07 -0700

    add iopid tune

---
+++ 1. iopid_tune_pid/iopid_tune.m
@@ -0,0 +1,18 @@
+% First we need the plant transfer function.
+s=tf('s');
+p_tf=1.0263/(0.71*s+1)/s;
+
+%% tune
+% opts = pidtuneOptions('CrossoverFrequency',32,'PhaseMargin',90);
+[C, info] = pidtune(p_tf, 'pid')
+
+%% controller transfer function
+ioc_tf=C.Kp+C.Ki/s+C.Kd*s;
+
+%% open loop
+iosys_tf=ioc_tf*p_tf;
+figure(1)
+%bode(iosys_tf)
+%grid on
+margin(iosys_tf)
+
