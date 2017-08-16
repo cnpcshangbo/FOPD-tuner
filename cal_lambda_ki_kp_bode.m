f47f3ea97ae065c7783b1f3b4a3121dad09aa1fd
Bo Shang <cnpcshangbo@gmail.com>
2017-08-16 14:43:51 -0700

    Improve cell names

--- cal_lambda_ki_kp_bode.m
+++ cal_lambda_ki_kp_bode.m
@@ -1,6 +1,6 @@
 %%% This is comments. Please contact cnpcshangbo@gmail.com
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% The setting parameters.
+%% Setting parameters and plotting ki against lambda
 lamda=-1:0.0001:-0;
 k=1.0584;%plant parameters. p(s)=\frac{k}{\tau s+1}\frac{1}{s}
 tao=0.63;%plant parameters
@@ -25,7 +25,7 @@ plot(lamda, ki0,'r-.',lamda, ki1,'b-', lamda, ki2,'g-')
 legend('ki0','ki1','ki2')
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %\lambda=-0.9855;ki2=0.2431
-%% solution 1
+%% Solution 1
 % To verify the crosspoint with ki1
 lamda=-0.97; % update lambda value here, make sure the value is negative.
 a=tao*omega.^(-2*lamda);
@@ -35,7 +35,7 @@ ki1=(-b+(b.^2-4*a*c).^0.5)./(2*a);
 disp('ki1:');
 disp(ki1);
 
-%% verify the controller 1
+%% Verify the controller 1
 ki=ki1;
 s=fotf('s');
 Jomega=((1+ki*omega.^(-lamda).*cos(pi*lamda/2))^2+(ki*omega.^(-lamda).*sin(pi*lamda/2))^2)^0.5;
@@ -48,7 +48,7 @@ figure(2)
 bode(sys_tf);
 grid on
 
-%% 2
+%% Solution 2
 lamda=-0.9855;
 a=tao*omega.^(-2*lamda);
 b=-lamda.*sin(pi*lamda/2).*omega.^(-lamda-1)*(omega^2*tao^2+1)+2*tao*omega.^(-lamda).*cos(pi*lamda/2);
@@ -56,7 +56,7 @@ c=tao;
 ki2=(-b-(b.^2-4*a*c).^0.5)./(2*a); %ki
 disp('ki2:');disp(ki2);
 
-%% verify the controller 2
+%% Verify the controller 2
 ki=ki2;
 s=fotf('s');
 Jomega=((1+ki*omega.^(-lamda).*cos(pi*lamda/2))^2+(ki*omega.^(-lamda).*sin(pi*lamda/2))^2)^0.5;
